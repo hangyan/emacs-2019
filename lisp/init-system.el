e7cefd23977582d64de09effc7b45f794bf20df0
Hang Yan <hangyan@alauda.io>
2019-11-25 11:21:52 +0800

    Update

--- lisp/init-system.el
+++ lisp/init-system.el
@@ -61,14 +61,14 @@
 
 
 
-(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;;(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+;;(smex-initialize)
+;;(global-set-key (kbd "M-x") 'smex)
+;;(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
-(ido-mode t)
-(setq ido-enable-flex-matching t
-      ido-use-virtual-buffers t)
+;;(ido-mode t)
+;; (setq ido-enable-flex-matching t
+;;       ido-use-virtual-buffers t)
 
 
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
@@ -105,5 +105,11 @@
   )
 
 
+
+;; helm
+(require 'helm-config)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
 
 (provide 'init-system)
