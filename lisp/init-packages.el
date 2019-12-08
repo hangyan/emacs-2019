f759dda364df135acac33fba550a62cd0f91574c
Hang Yan <hangyan@alauda.io>
2019-12-08 15:41:46 +0800

    Update

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -36,6 +36,7 @@
                           rvm
                           smex
                           solarized-theme
+			  treemacs
                           web-mode
                           writegood-mode
                           yaml-mode)
