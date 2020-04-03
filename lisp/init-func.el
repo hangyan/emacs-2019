6516dffa58063c311ee6b5aff44d74801cd9a95c
Hang Yan <hangyan@alauda.io>
2020-04-04 02:02:00 +0800

    Try magit

--- lisp/init-func.el
+++ lisp/init-func.el
@@ -353,6 +353,39 @@ Version 2017-05-04"
                         (kill-buffer buffer)))
                     (message "Killed %i buffer(s)." count))))
 
+
+
+;; functions copied from https://github.com/alphapapa/unpackaged.el
+;;;###autoload
+;;; Open a magit-status buffer and close the other window so only Magit is visible. If a file was visited in the buffer that was active when this command was called, go to its unstaged changes section.
+(defun unpackaged/magit-status ()
+  "Open a `magit-status' buffer and close the other window so only Magit is visible.
+If a file was visited in the buffer that was active when this
+command was called, go to its unstaged changes section."
+  (interactive)
+  (let* ((buffer-file-path (when buffer-file-name
+                             (file-relative-name buffer-file-name
+                                                 (locate-dominating-file buffer-file-name ".git"))))
+         (section-ident `((file . ,buffer-file-path) (unstaged) (status))))
+    (call-interactively #'magit-status)
+    (delete-other-windows)
+    (when buffer-file-path
+      (goto-char (point-min))
+      (cl-loop until (when (equal section-ident (magit-section-ident (magit-current-section)))
+                       (magit-section-show (magit-current-section))
+                       (recenter)
+                       t)
+               do (condition-case nil
+                      (magit-section-forward)
+                    (error (cl-return (magit-status-goto-initial-section-1))))))))
+
+;;;###autoload
+(defun unpackaged/magit-save-buffer-show-status ()
+  "Save buffer and show its changes in `magit-status'."
+  (interactive)
+  (save-buffer)
+  (unpackaged/magit-status))
+
 (provide 'init-func)
 
 ;;; Init-func.el ends here
